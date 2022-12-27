5a68129cc687c20ffd07323ebe5d9391141fc3c6
Lee dong ho <dong963939@gmail.com>
2022-12-27 17:48:20 +0900

    ➕ Banner 변경 및 수정.

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -43,14 +43,16 @@ export default function Section(): JSX.Element {
                                 <SwiperSlide key={idx}>
                                     <div className="Banner w-screen">
                                         <img src={item.src} alt={item.alt} />
-                                        <div className="Banner_text absolute left-1/2 lg:left-1/3 bottom-1/2 z-20">
+                                        <div className="Banner_text absolute left-1/2 xl:left-1/3 bottom-1/2 z-20">
                                             <Fade bottom>
-                                                <h2 className="text-white text-left text-3xl md:text-4xl lg:text-5xl font-semibold mb-8 tracking-tighter line-clamp-1">{item.h2}</h2>
-                                                <p className="text-white text-left text-lg lg:text-xl tracking-tighter">{item.p1}<br />{item.p2}</p>
-                                                <a
-                                                    className="absolute left-0 hover:bg-white hover:text-black transition px-5 py-3 rounded-lg border border-white text-white tracking-wider font-semibold text-sm sm:text-base mt-8 cursor-pointer"
-                                                    onClick={() => router.push(`/Detail/${idx}`)}
-                                                >더 알아보기</a>
+                                                <h2 className="text-white text-center xl:text-left text-3xl md:text-4xl xl:text-5xl font-semibold mb-10 tracking-tighter line-clamp-1">{item.h2}</h2>
+                                                <p className="text-white text-center xl:text-left text-sm xl:text-lg tracking-tighter">{item.p1}<br />{item.p2}</p>
+                                                <div className="text-center xl:text-left -mb-16 mt-10 lg:-ml-5 xl:ml-0">
+                                                    <a
+                                                        className="hover:bg-white hover:text-black transition px-5 py-3 rounded-lg border border-white text-white tracking-wider font-semibold text-sm sm:text-base cursor-pointer"
+                                                        onClick={() => router.push(`/Detail/${idx}`)}
+                                                    >더 알아보기</a>
+                                                </div>
                                             </Fade>
                                         </div>
                                     </div>
--- components/Banner/Banner_data.ts
+++ components/Banner/Banner_data.ts
@@ -3,8 +3,8 @@ export const product = [
         "src": "https://raw.githubusercontent.com/light9639/ImgStorage/main/shoestore/index/mainBanner01.png",
         "alt": "mainBanner01",
         "h2": "한계 없는 플레이, 끝 없는 즐거움",
-        "p1": "득점이 쏟아지는 NIKELAND 농구를 친구들과 함께 즐겨보세요.",
-        "p2": "새로운 팀 그리고 장비를 통해 정면 승부를 펼치거나 덩크슛 콘테스트에 도전해 보세요.",
+        "p1": "득점이 쏟아지는 농구를 친구들과 함께 즐겨보세요.",
+        "p2": "새로운 팀과 장비를 통해 정면 승부를 펼치거나 덩크슛 콘테스트에 도전해 보세요.",
     },
     {
         "src": "https://raw.githubusercontent.com/light9639/ImgStorage/main/shoestore/index/mainBanner02.png",
@@ -21,10 +21,10 @@ export const product = [
         "p2": "안정감을 제공하는 나이키 줌X 제가마를 경험해 보세요.",
     },
     {
-        "src": "https://raw.githubusercontent.com/light9639/ImgStorage/main/shoestore/index/mainBanner04.png",
+        "src": "https://raw.githubusercontent.com/light9639/ImgStorage/main/shoestore/index/mainBanner04.jpg",
         "alt": "mainBanner04",
-        "h2": "에어 조던 37",
-        "p1": "최첨단의 혁신과 소재를 더한 에어 조던 37과 함께",
-        "p2": "더 높이, 더 멀리 나아가 보세요.",
+        "h2": "조던 루카 1",
+        "p1": "루카는 승리가 불투명한 상황일 떄도 경기의 흐름을",
+        "p2": " 바꾸어 기회를 만들어내는 선수입니다.",
     }
 ]
